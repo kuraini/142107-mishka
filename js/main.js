6fc8afb073c7d5fd2d34e9c7d87c555beabda93e
Svetlana Borisova <kuraini@yandex.ru>
2017-10-13 14:48:45 +0300

    Вставлена карта

--- js/main.js
+++ js/main.js
@@ -12,3 +12,28 @@ navToggle.addEventListener("click", function() {
     navMain.classList.remove("main-nav--opened");
   }
 });
+
+ymaps.ready(init);
+
+function init() {
+  var myMap = new ymaps.Map("map", {
+    center: [59.93863106417265,30.3230545],
+    zoom: 17,
+    controls: []
+  });
+
+  var myPlacemark = new ymaps.Placemark([59.93863106417265,30.3230545],
+  {
+    hintContent: "Наш офис"
+  },
+  {
+    iconLayout: "default#image",
+    iconImageHref: "img/icon/icon-map-pin.svg",
+    iconImageSize: [66, 101],
+    iconImageOffset: [-25, -85]
+  });
+
+  myMap.geoObjects.add(myPlacemark);
+  myMap.controls.add("zoomControl");
+  myMap.behaviors.disable("scrollZoom");
+}
